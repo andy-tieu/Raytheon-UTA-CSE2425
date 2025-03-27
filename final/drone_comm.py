e20c6c77a6283f48e2d3ccf97f6afa52d8b3fe52
Andy Tieu <andytieu325@gmail.com>
2025-03-27 16:58:51 -0500

    Added communications program

---
+++ final/drone_comm.py
@@ -0,0 +1,35 @@
+"""
+RAYTHEON CSE TEAM 2024-2025
+
+PROGRAM TO RUN COMMUNICATION BETWEEN UAV AND UGV
+"""
+
+""" START OF IMPORTS """
+
+# Usual Python imports
+import socket
+
+""" END OF IMPORTS """
+
+
+
+""" START OF FUNCTIONS """
+
+# UGV_PORT should be constant
+def client_init(UGV_IP, UGV_PORT):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((UGV_IP, UGV_PORT))
+
+    return client
+
+def send_msg(client, msg):
+    # Send a message to UGV (in this case, the UAV's coordinates)
+    client.send(msg.encode())
+
+    # Get response from UGV if any
+    response = client.recv(1024).decode()
+    print(f"Response from UGV: {response}")
+
+    return response # In case any info is needed
+
+""" END OF FUNCTIONS """
