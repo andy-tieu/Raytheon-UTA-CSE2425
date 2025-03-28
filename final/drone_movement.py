4ee9975cec07068f174f1076b480ae7261048ede
Andy Tieu <andytieu325@gmail.com>
2025-03-28 01:40:33 -0500

    Added start of movement file

---
+++ final/drone_movement.py
@@ -0,0 +1,21 @@
+"""
+RAYTHEON CSE TEAM 2024-2025
+
+PROGRAM TO RUN UAV DRONE MOVEMENT
+"""
+
+""" START OF IMPORTS """
+
+# Dependencies; need to be installed
+from dronekit import connect, VehicleMode, LocationGlobal
+
+""" END OF IMPORTS """
+
+
+
+""" START OF FUNCTIONS """
+
+# TODO:
+# ADD CODE HERE TO PERFORM MOVEMENT COMMANDS
+
+""" END OF FUNCTIONS"""
